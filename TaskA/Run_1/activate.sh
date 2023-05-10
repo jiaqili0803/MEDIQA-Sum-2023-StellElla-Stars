c7ecf739eefc126f5cff22b1b36c0d687a784840
Ella.Li <40383000+jiaqili0803@users.noreply.github.com>
2023-05-10 01:11:50 -0400

    Add files via upload

---
+++ TaskA/Run_1/activate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source StellEllaStars_taskA_venv/bin/activate
+
---
+++ TaskA/Run_1/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -m venv StellEllaStars_taskA_venv
+source StellEllaStars_taskA_venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
