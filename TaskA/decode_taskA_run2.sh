25de6309e750a2948ce5117807be2b004544350a
Ella.Li <40383000+jiaqili0803@users.noreply.github.com>
2023-05-10 18:11:38 -0400

    Update decode_taskA_run2.sh

--- TaskA/decode_taskA_run2.sh
+++ TaskA/decode_taskA_run2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source activate.sh
-python run_2.py --train_csv_file $1 --valid_csv_file $2 --test_csv_file $3 --output_csv_file taskA_StellEllaStars_run2_mediqaSum.csv
+python3 run_2.py --train_csv_file $1 --valid_csv_file $2 --test_csv_file $3 --output_csv_file taskA_StellEllaStars_run2_mediqaSum.csv
 
